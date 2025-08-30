1a8eb84cd00e628462155407dbdb8f2bae302e22
David Lewis <davidlewisp@gmail.com>
2025-08-29 19:06:24 -0600

    Test: automatic formatting on commit

---
+++ test-formatting.ts
@@ -0,0 +1,11 @@
+// Test file with intentionally bad formatting
+function testFunction() {
+  console.log('This has bad formatting');
+  const x = 5;
+  return x;
+}
+
+// Extra whitespace
+const y = 10;
+
+export { testFunction, y };
